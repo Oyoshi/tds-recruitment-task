5635fba57aebaee0788305a4ac4862152b8e46d6
Oyoshi <rafal.apriasz@gmail.com>
2026-01-22 19:28:29 +0100

    init project using React, Vite, TS and ShadcnUI

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1>Hello World</h1>
+      <Select>
+        <SelectTrigger className="w-full max-w-48">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
+
+export default App;
