f8004da01420864f7d317eb4d7d3811d8b558e66
Oyoshi <rafal.apriasz@gmail.com>
2026-01-22 21:03:02 +0100

    feat: implement bi-directional data flow change within the form

--- src/components/currencies/CurrencyConverterForm.tsx
+++ src/components/currencies/CurrencyConverterForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
 import { CurrencyInput } from './CurrencyInput';
 
@@ -7,7 +8,34 @@ const mockCurrencies = [
   { code: 'USD', name: 'US Dollar' },
 ];
 
+const EXCHANGE_RATE = 0.34;
+
 export function CurrencyConverterForm() {
+  /* Decided to use separated 4 states to represent bi-drectional data flow just for the simplicity.
+   * Normally, I would consider using a more robust state management approach (like useReducer, zustand)
+   * or a form library for handling complex forms.
+   */
+  const [firstAmount, setFirstAmount] = useState('1.00');
+  const [firstCurrency, setFirstCurrency] = useState('PLN');
+  const [secondAmount, setSecondAmount] = useState('0.34');
+  const [secondCurrency, setSecondCurrency] = useState('EUR');
+
+  const handleFirstChange = (value: string) => {
+    setFirstAmount(value);
+    // @todo - move it to a utility function
+    const parsedValue = parseFloat(value);
+    const numericValue = isNaN(parsedValue) ? 0 : Math.max(0, parsedValue);
+    setSecondAmount((numericValue * EXCHANGE_RATE).toFixed(2));
+  };
+
+  const handleSecondChange = (value: string) => {
+    setSecondAmount(value);
+    // @todo - move it to a utility function
+    const parsedValue = parseFloat(value);
+    const numericValue = isNaN(parsedValue) ? 0 : Math.max(0, parsedValue);
+    setFirstAmount((numericValue / EXCHANGE_RATE).toFixed(2));
+  };
+
   return (
     <Card className="w-full max-w-md shadow-lg">
       <CardHeader className="pb-2 text-center">
@@ -20,8 +48,22 @@ export function CurrencyConverterForm() {
       </CardHeader>
       <CardContent>
         <form className="flex flex-col gap-4 mb-4">
-          <CurrencyInput prefix="first" currencies={mockCurrencies} />
-          <CurrencyInput prefix="second" currencies={mockCurrencies} />
+          <CurrencyInput
+            prefix="first"
+            currencies={mockCurrencies}
+            value={firstAmount}
+            onValueChange={handleFirstChange}
+            currency={firstCurrency}
+            onCurrencyChange={setFirstCurrency}
+          />
+          <CurrencyInput
+            prefix="second"
+            currencies={mockCurrencies}
+            value={secondAmount}
+            onValueChange={handleSecondChange}
+            currency={secondCurrency}
+            onCurrencyChange={setSecondCurrency}
+          />
         </form>
         <p className="text-xs text-center text-muted-foreground">
           Last updated: {new Date().toLocaleTimeString()}
--- src/components/currencies/CurrencyInput.tsx
+++ src/components/currencies/CurrencyInput.tsx
@@ -9,26 +9,42 @@ import {
 import { Separator } from '@/components/ui/Separator';
 
 const BORDERLESS_INPUT_CLASSES = 'border-0 bg-transparent shadow-none';
-const CURRENCY_VALUE_INPUT_PLACEHOLDER = '0.00';
+const CURRENCY_VALUE_INPUT_PLACEHOLDER = '1.00';
 const CURRENCY_CODE_INPUT_PLACEHOLDER = 'USD';
 
+type InputValueChangeHandler = (val: string) => void;
+
 interface CurrencyInputProps {
-  prefix: string;
   currencies: { code: string; name: string }[];
+  prefix: string;
+  value: string;
+  onValueChange: InputValueChangeHandler;
+  currency: string;
+  onCurrencyChange: InputValueChangeHandler;
 }
 
-export function CurrencyInput({ currencies, prefix }: CurrencyInputProps) {
+export function CurrencyInput({
+  currencies,
+  prefix,
+  value,
+  onValueChange,
+  currency,
+  onCurrencyChange,
+}: CurrencyInputProps) {
   return (
     <div className="flex h-12 w-full items-center rounded-md border border-input bg-background p-2 shadow-sm">
       <Input
         type="number"
         className={BORDERLESS_INPUT_CLASSES}
         placeholder={CURRENCY_VALUE_INPUT_PLACEHOLDER}
+        value={value}
+        onChange={(e) => onValueChange(e.target.value)}
         aria-label={`${prefix} field to input currency value`}
         id={`${prefix}-currency-value-input`}
+        min="0"
       />
       <Separator orientation="vertical" className="h-full" />
-      <Select>
+      <Select value={currency} onValueChange={onCurrencyChange}>
         <SelectTrigger
           className={BORDERLESS_INPUT_CLASSES}
           aria-label={`$prefix} field to select currency`}
